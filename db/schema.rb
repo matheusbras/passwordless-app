0c17d84ea637c3955956d3334274faaf2b0228bb
Matheus Bras <bras.matheus@gmail.com>
2013-03-31 13:34:50 -0300

    Generated User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130331163155) do
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "access_token"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
 end

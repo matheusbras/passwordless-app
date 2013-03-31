9f965d07652d8c8b3ca174f610d8e0b99c26430b
Matheus Bras <bras.matheus@gmail.com>
2013-03-31 13:19:48 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passwordless::Application.config.secret_token = 'eb63395406d9ad1adf3f34c1057b2ccd0d4112caa3ff4f0ef3fcb2b1b6cb2f70e7ae4939a94a1b9a28b8d4484bd1d200edf6734d9ae5849d9039312f77bf8c55'

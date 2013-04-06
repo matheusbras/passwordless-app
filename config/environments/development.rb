69c84edb25b2310ec1355b7c97a9cfdb977c58b8
Matheus Bras <bras.matheus@gmail.com>
2013-04-06 12:02:11 -0300

    Changed the default host url on development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,5 @@ Passwordless::Application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.default_url_options = { :host => 'dev.passwordlessapp.com.br:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end

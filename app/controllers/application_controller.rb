73a962d6219552599bb9c32b435432a1e2ab451a
Matheus Bras <bras.matheus@gmail.com>
2013-04-06 13:51:03 -0300

    Added authentication helpers on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
+# encoding: UTF-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
   ensure_security_headers # See more: https://github.com/twitter/secureheaders
+  helper_method :current_user, :user_signed_in?
 
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      session.delete(:user_id)
+      nil
+  end
+
+  def user_signed_in?
+    !current_user.nil?
+  end
+
+  def authenticate!
+    user_signed_in? || redirect_to(root_url, notice: "Você precisa estar autenticado...")
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe ApplicationController do
+  let!(:user) { create(:user) }
+
+  describe "current_user helper" do
+    context "with user logged in" do
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "returns the logged user" do
+        controller.send(:current_user).should eq(user)
+      end
+    end
+
+    context "without user logged in" do
+      it "returns nil" do
+        controller.send(:current_user).should be_nil
+      end
+    end
+  end
+
+  describe "user_signed_in? helper" do
+    context "with user logged in" do
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "returns true" do
+        controller.send(:user_signed_in?).should be_true
+      end
+    end
+
+    context "without user logged in" do
+      it "returns false" do
+        controller.send(:user_signed_in?).should be_false
+      end
+    end
+  end
+
+  describe "authenticate! filter" do
+    context "with user logged in" do
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "returns true" do
+        controller.send(:authenticate!).should be_true
+      end
+    end
+  end
+end

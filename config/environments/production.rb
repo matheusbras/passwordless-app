7c7b06d135ac2ba1b8e5e4c7b8853711cc6e54c3
Matheus Bras <bras.matheus@gmail.com>
2013-04-11 09:48:10 -0300

    Added sendgrid configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,14 @@ Passwordless::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'passwordless-app.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
